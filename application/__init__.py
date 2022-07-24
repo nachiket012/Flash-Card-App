566e02c2ca24dcb63d0f6b05fed8a72ff4ecd174
nachiket012 <89441920+nachiket012@users.noreply.github.com>
2022-07-25 00:23:48 +0530

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -14,9 +14,9 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mydb.sqlite3"
 
-app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECRET_KEY'] = os.environ['secret-key']
 app.config['SECURITY_REGISTERABLE'] = True
-app.config['SECURITY_PASSWORD_SALT'] = 'yOr04oAPMwFcLXBU3tX4SjFxXIxm8yFW4EZ7zpBF8ZP2WXJoUsJnPobuEKRX2NZOH1jypQVdtT66ewpi'
+app.config['SECURITY_PASSWORD_SALT'] = os.environ['password-salt']
 app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
 app.config['SECURITY_POST_LOGIN_VIEW'] = '/'
 app.config['SECURITY_POST_REGISTER_VIEW'] = '/login'
@@ -27,8 +27,8 @@ app.config["CELERY_RESULT_BACKEND"] = "redis://localhost:6379/2"
 app.config["MAIL_DEFAULT_SENDER"] = "email@flashcard.com"
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 2525
-app.config['MAIL_USERNAME'] = 'eae8829a9e9e74'
-app.config['MAIL_PASSWORD'] = '759eb52a4b4e80'
+app.config['MAIL_USERNAME'] = os.environ['mail-username']
+app.config['MAIL_PASSWORD'] = os.environ['mail-password']
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
